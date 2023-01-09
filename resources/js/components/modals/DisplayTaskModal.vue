c3321fbd012be3dcb86fec95359c1eeb2d15e3c2
Hini Majesty <19369546+hinimajesty@users.noreply.github.com>
2023-01-09 07:26:05 +0000

    [wip] on modal setup

---
+++ resources/js/components/modals/DisplayTaskModal.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+
+        <div>
+            {{ description }}
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            title: String, 
+            description: String
+        }
+    }
+</script>
