9aa63b0bd0873c06f8c39930fdb7c463234d9564
Hini Majesty <19369546+hinimajesty@users.noreply.github.com>
2023-01-11 18:59:53 +0000

    [Fix] close form on cancel

--- resources/js/components/AddTask.vue
+++ resources/js/components/AddTask.vue
@@ -1,17 +1,19 @@
 <template>
     <div> <!--root-->
 
-       <div v-show="showCreateTaskBtn" class="addtask">
+        <div v-show="showCreateTaskBtn" class="addtask">
             <div @click="showCreateTaskForm" class="addtask__btn addtask__btn--addtask">+ Add a task</div>
-       </div>
+        </div>
 
-      <div v-show="showForm" class="form">
+        <div v-show="showForm" class="form">
             <form @submit.prevent="addTask">
-            
-                <textarea v-model="newTitle" class="form__input form__input--nodrag" name="" rows="1" placeholder="Tasks title" required></textarea>
-                <br/><br/>
-                
-                <textarea v-model="newDescription" class="form__input form__input--nodrag" name="" rows="2" placeholder="Describe the task" required></textarea>
+
+                <textarea v-model="newTitle" class="form__input form__input--nodrag" name="" rows="1"
+                    placeholder="Tasks title" required></textarea>
+                <br /><br />
+
+                <textarea v-model="newDescription" class="form__input form__input--nodrag" name="" rows="2"
+                    placeholder="Describe the task" required></textarea>
 
                 <div class="form__control">
                     <button class="form__btn form__btn--addtask">Add Task</button>
@@ -21,37 +23,37 @@
 
         </div>
 
-    </div>  <!--end root-->
+    </div> <!--end root-->
 </template>
 
 <script>
-    export default {
-        data(){
-            return {
-                newTitle: '',
-                newDescription: '', 
-                showForm : false, 
-            }
-        },
-        props: {
-        },
-        methods : {
-            showCreateTaskForm() {
-                this.showForm = true;
-            }, 
-            addTask(){
-                this.$emit('addNewTask',this.newTitle, this.newDescription)
-
-                this.showForm = false;
-                this.newTitle = '';
-                this.newDescription = '';
-            }, 
-            cancelCreateTask(){
-                this.showForm = false;
-            }
-        }, 
-        computed: {
-            showCreateTaskBtn (){ return ! this.showForm }
+export default {
+    data() {
+        return {
+            newTitle: '',
+            newDescription: '',
+            showForm: false,
         }
+    },
+    props: {
+    },
+    methods: {
+        showCreateTaskForm() {
+            this.showForm = true;
+        },
+        addTask() {
+            this.$emit('addNewTask', this.newTitle, this.newDescription)
+
+            this.showForm = false;
+            this.newTitle = '';
+            this.newDescription = '';
+        },
+        cancelCreateTask() {
+            this.showForm = false;
+        }
+    },
+    computed: {
+        showCreateTaskBtn() { return !this.showForm }
     }
+}
 </script>
